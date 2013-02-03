e2511852840b8b21d0a31e0e9ac9909f908050a1
nickdanger3d <nick@bonfatti.net>
2013-02-02 23:44:46 -0500

    Update subpotato.py

--- subpotato.py
+++ subpotato.py
@@ -5,11 +5,10 @@ import simplejson as json
 import urllib2
 import feedparser
 
-def init() : 
-    defaultUrl="https://couchpotato/"
+defaultUrl="https://couchpotato/"
     #get this from defaultUrl/docs
-    defaultKey=
-    defaultFeed="http://http://rss.imdb.com/user/ur#######/watchlist"
+defaultKey=
+defaultFeed="http://http://rss.imdb.com/user/ur#######/watchlist"
 
 def processFileList(filesToCopy) :
     #just print for now, eventually send to rsync
@@ -20,26 +19,28 @@ def subpotato(URL_BASE, API_KEY, FEED_URL) :
     feedDict = feedparser.parse(FEED_URL)["entries"]
 
     #Make the set of  movie names and imdb numbers
-    movieSet = set()
+    movies = []
     feedLength = len(feedDict)
     for j in range(feedLength):
         imdbID=feedDict[j]['link'][26:35]
-        movieSet.append({'title' : feedDict[j]['title'], 'id' : imdbID})
+        #print imdbID
+        #print feedDict[j]['title']
+        movies.append({'title' : feedDict[j]['title'], 'id' : imdbID})
 
     #get the set of movie names available from couchpotato
-    releases= set()
-    for k in range(feedLength):
-        url=URL_BASE+"/api/"+API_KEY+"/"+"movie.get?id="+movieSet[k]['id']
+    releases= []
+    for movie in movies:
+        url=URL_BASE+"/api/"+API_KEY+"/"+"movie.get?id="+movie['id']
         json_string = urllib2.urlopen(url).read()
         the_data=json.loads(json_string)
         releases.append(the_data["movie"]["releases"])
 
     #get filenames to be synced
-    filesList = set()
+    filesList = []
     for release in releases:
         for x in release :
-            for file in x["files"] :
-                filesList.append( y["path"] )
+            for filename in x["files"] :
+                filesList.append( filename["path"] )
     
     #process the list of files
     processFileList(filesList)
